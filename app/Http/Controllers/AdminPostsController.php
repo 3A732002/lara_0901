192b8540ebdb4953b07c6548e568db28431eed20
ma yu cen <sheehan010507@hotmail.com>
2020-12-02 00:57:39 +0800

    練習 5-1： 將表單送過來的資料用 Model 寫入資料庫

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class AdminPostsController extends Controller
 {
@@ -24,8 +25,8 @@ class AdminPostsController extends Controller
 
         return view('admin.posts.edit', $data);
     }
-    public function store()
+    public function store(Request $request)
     {
-
+        Post::create($request->all());
     }
 }
